cccd22b99058d311df66fdba6fd7159b8032a58a
Sepkohn <50259520+Sepkohn@users.noreply.github.com>
2019-05-04 10:21:08 +0200

    Ajout des fichiers

---
+++ Ecran.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import javax.swing.*;
+
+import javafx.scene.layout.Background;
+
+public class Ecran extends JFrame{
+	
+	JButton cancel = new JButton("O");
+	JButton galerie = new JButton("Galerie");
+	JButton contact = new JButton("Contacts");
+	JButton undo = new JButton("Back");
+	
+	JPanel south = new JPanel();
+	JPanel centre = new JPanel();
+	
+	public Ecran () {
+		
+		
+		south.add(cancel);
+		south.add(undo, BorderLayout.LINE_END);
+		
+		centre.add(galerie);
+		centre.add(contact);
+		
+		//centre.setBackground(Color.GREEN);
+		centre.setOpaque(false);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		setTitle("Smartphone");
+		
+		getContentPane().setBackground(Color.BLACK);
+		
+		add(south, BorderLayout.SOUTH);
+		add(centre, BorderLayout.CENTER);
+		
+		
+		
+		
+	}
+
+
+}
---
+++ lancement.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+
+public class lancement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Ecran smartphone = new Ecran();
+		
+		smartphone.setVisible(true);
+		
+		smartphone.setSize(500,900);
+		
+		
+		
+		
+		
+	}
+
+}
