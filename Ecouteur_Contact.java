79c2db5771fb3f2727dcf0c89468884d188fa1aa
K4RM0 <karmo1997@gmai.com>
2019-05-10 14:49:00 +0200

    lol

--- Ecouteur_Contact.java
+++ Ecouteur_Contact.java
@@ -1,43 +1,46 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 
-public class Ecouteur_Contact extends JFrame {
+public class Ecouteur_Contact extends MouseAdapter {
 
     private JButton ButtonAdd = new JButton("Ajouter");
     private JButton ButtonClose = new JButton("Fermer");
     private JPanel panel_south = new JPanel();
     private JPanel panel_north = new JPanel();
     private JPanel panel_center = new JPanel() ;
-    private JTextField panel_titre = new JTextField();
 
 
-    public Ecouteur_Contact() {
 
-        //Ecouteur ecouteur = new Ecouteur() ;
-
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        // ButtonAdd.addActionListener();
-        // ButtonAdd.setActionCommand("toto");
-
-        panel_south.add(ButtonAdd);
-        panel_south.add(ButtonClose);
-
-        add(panel_south, BorderLayout.SOUTH);
-        add(panel_north, BorderLayout.NORTH);
-        add(panel_titre, BorderLayout.NORTH);
-        add(panel_center, BorderLayout.CENTER) ;
+    public  Ecouteur_Contact() {
 
 
-        ButtonAdd.addActionListener(new Ecouteur_1());
 
-        pack();
+
     }
+
+    @Override
+    public void mouseClicked(MouseEvent event){
+
+
+        JFrame Window = new JFrame();
+        Window.setVisible(true);
+        Window.setSize(500,900);
+        panel_south.getAccessibleContext();
+        panel_south.add(ButtonAdd);
+        panel_south.add(ButtonClose) ;
+
+    }
+
 
     //public class Ecouteur_1 implements ActionListener {
 
       //  if()
 
 
-return Ecouteur_Contact ;
+
 }
