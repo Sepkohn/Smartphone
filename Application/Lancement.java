63b86685e6781667c6120cd29b1b21c650b080dc
K4RM0 <karmo1997@gmai.com>
2019-05-30 10:59:54 +0200

    jespere ne pas faire des conneries...

---
+++ Application/Lancement.java
@@ -0,0 +1,10 @@
+public class Lancement {
+    public Lancement() {
+    }
+
+    public static void main(String[] args) {
+        Ecran smartphone = new Ecran();
+        smartphone.setVisible(true);
+        smartphone.setSize(500, 900);
+    }
+}
