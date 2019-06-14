962d1ae76b0085911e0e4cd63387418ef631981a
K4RM0 <karmo1997@gmai.com>
2019-06-14 10:52:37 +0200

    javadoc....

---
+++ Test/Test/Ecran_GalerieTest.java
@@ -0,0 +1,15 @@
+package Test;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class Ecran_GalerieTest {
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void misAjour() {
+    }
+}
